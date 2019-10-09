50f298504d273917b1dd777e7d1688d5a7c4ddbc
Jean Armand Nyilimbibi <jean.nyilimbibi@gmail.com>
2019-10-09 16:18:20 +0200

    Added OAS generation on REST configuration

--- sample-json-validation-api/src/main/java/com/github/jeannyil/fuse/jsonvalidation/routes/SampleJsonValidationApiRoute.java
+++ sample-json-validation-api/src/main/java/com/github/jeannyil/fuse/jsonvalidation/routes/SampleJsonValidationApiRoute.java
@@ -2,11 +2,12 @@ package com.github.jeannyil.fuse.jsonvalidation.routes;
 
 import javax.ws.rs.core.MediaType;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,9 @@ import com.github.jeannyil.fuse.common.models.ErrorResponse;
 public class SampleJsonValidationApiRoute extends RouteBuilder {
 
 	private static String logName = SampleJsonValidationApiRoute.class.getName();
+
+	@Autowired
+	BuildProperties buildProperties;
 	
 	@Override
 	public void configure() throws Exception {
@@ -43,21 +47,37 @@ public class SampleJsonValidationApiRoute extends RouteBuilder {
 			.enableCORS(true)
 			.bindingMode(RestBindingMode.off) // RESTful responses will be explicitly marshaled for logging purposes
 			.dataFormatProperty("prettyPrint", "true")
-			// Add the context path for the OpenAPI documentation:
-            //.apiContextPath("/openapi.json")
+			.scheme("http")
+			.host("0.0.0.0")
+			.port("{{server.port}}")
+			.contextPath("/")
+			// Add information for the generated Open API Specification
+            .apiContextPath("/validateMembershipJSON/api-doc")
+            	.apiContextRouteId("api-doc-route")
+				.apiProperty("api.title", "Sample JSON Validation API")
+				.apiProperty("api.description", "A simple API to test the Camel json-schema-validator component")
+				.apiProperty("api.version", buildProperties.getVersion())
+				.apiProperty("api.contact.name", "Jean Nyilimbibi")
+				.apiProperty("api.license.name", "MIT License")
+				.apiProperty("api.license.url", "https://opensource.org/licenses/MIT")
+                .apiProperty("cors", "true")
 		;
 		
 		/**
 		 * REST endpoint for the Service OpenAPI document 
-		 */
+		 
 		rest().id("openapi-document-restapi")
-			.consumes(MediaType.APPLICATION_JSON)
 			.produces(MediaType.APPLICATION_JSON)
 			
 			// Gets the OpenAPI document for this service
 			.get("/validateMembershipJSON/openapi.json")
 				.id("get-openapi-doc-route")
 				.description("Gets the OpenAPI document for this service")
+				.responseMessage()
+					.code(HttpStatus.OK.value())
+					.message(HttpStatus.OK.getReasonPhrase())
+					.responseModel(com.github.jeannyil.fuse.common.models.ValidationResult.class)
+				.endResponseMessage()
 				.route()
 					.log(LoggingLevel.INFO, logName, ">>> ${routeId} - IN: headers:[${headers}] - body:[${body}]").id("log-openapi-doc-request")
 					.setHeader(Exchange.CONTENT_TYPE, constant("application/vnd.oai.openapi+json")).id("set-content-type")
@@ -67,12 +87,14 @@ public class SampleJsonValidationApiRoute extends RouteBuilder {
 					.log(LoggingLevel.INFO, logName, ">>> ${routeId} - OUT: headers:[${headers}] - body:[${body}]").id("log-openapi-doc-response")
 				.end()
 			.endRest()
-		;
+		; */
 		
 		/**
 		 * REST endpoint for the Sample JSON Validation RESTful API 
 		 */
 		rest().id("sample-json-validation-restapi")
+			.consumes(MediaType.APPLICATION_JSON)
+			.produces(MediaType.APPLICATION_JSON)
 				
 			// Validates a `Membership` JSON instance
 			.post("/validateMembershipJSON")
@@ -84,21 +106,29 @@ public class SampleJsonValidationApiRoute extends RouteBuilder {
 					.description("A `Membership` JSON instance to be validated.")
 					.dataType("string")
 					.required(true)
+					.example(MediaType.APPLICATION_JSON,
+							 "{\n    \"requestType\": \"API\",\n    \"requestID\": 5948,\n    \"memberID\": 85623617,\n    \"status\": \"A\",\n    \"enrolmentDate\": \"2019-06-16\",\n    \"changedBy\": \"jeanNyil\",\n    \"forcedLevelCode\": \"69\",\n    \"vipOnInvitation\": \"Y\",\n    \"startDate\": \"2019-06-16\",\n    \"endDate\": \"2100-06-16\"\n}")
 				.endParam()
 				.responseMessage()
 					.code(HttpStatus.OK.value())
 					.message(HttpStatus.OK.getReasonPhrase())
 					.responseModel(com.github.jeannyil.fuse.common.models.ValidationResult.class)
+					.example(MediaType.APPLICATION_JSON, 
+							 "{\n    \"validationResult\": {\n        \"status\": \"OK\"\n    }\n}")
 				.endResponseMessage()
 				.responseMessage()
 					.code(HttpStatus.BAD_REQUEST.value())
 					.message(HttpStatus.BAD_REQUEST.getReasonPhrase())
 					.responseModel(com.github.jeannyil.fuse.common.models.ValidationResult.class)
+					.example(MediaType.APPLICATION_JSON, 
+							 "{\n    \"validationResult\": {\n        \"status\": \"KO\",\n        \"errorMessage\": \"6 errors found\"\n    }\n}")
 				.endResponseMessage()
 				.responseMessage()
 					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
 					.message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
 					.responseModel(ErrorResponse.class)
+					.example(MediaType.APPLICATION_JSON, 
+							 "{\n\t\"error\": {\n\t\t\"id\": \"500\",\n\t\t\"description\": \"Internal Server Error\",\n\t\t\"messages\": [\n\t\t\t\"java.lang.Exception: Mocked error message\"\n\t\t]\n\t}\n}")
 				.endResponseMessage()
 				// call the ValidateMembershipJSONRoute
 				.to("direct:validateMembershipJSON")
--- sample-xml-validation-api/src/main/java/com/github/jeannyil/fuse/xmlvalidation/routes/SampleXmlValidationApiRoute.java
+++ sample-xml-validation-api/src/main/java/com/github/jeannyil/fuse/xmlvalidation/routes/SampleXmlValidationApiRoute.java
@@ -2,11 +2,12 @@ package com.github.jeannyil.fuse.xmlvalidation.routes;
 
 import javax.ws.rs.core.MediaType;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,9 @@ import com.github.jeannyil.fuse.common.models.ErrorResponse;
 public class SampleXmlValidationApiRoute extends RouteBuilder {
 
 	private static String logName = SampleXmlValidationApiRoute.class.getName();
+
+	@Autowired
+	BuildProperties buildProperties;
 	
 	@Override
 	public void configure() throws Exception {
@@ -43,21 +47,37 @@ public class SampleXmlValidationApiRoute extends RouteBuilder {
 			.enableCORS(true)
 			.bindingMode(RestBindingMode.off) // RESTful responses will be explicitly marshaled for logging purposes
 			.dataFormatProperty("prettyPrint", "true")
-			// Add the context path for the OpenAPI documentation:
-            //.apiContextPath("/openapi.json")
+			.scheme("http")
+			.host("0.0.0.0")
+			.port("{{server.port}}")
+			.contextPath("/")
+            // Add information for the generated Open API Specification
+            .apiContextPath("/validateMembershipXML/api-doc")
+            	.apiContextRouteId("api-doc-route")
+                .apiProperty("api.title", "Sample XML Validation API")
+				.apiProperty("api.description", "A simple API to test the Camel XML validator component")
+				.apiProperty("api.version", buildProperties.getVersion())
+				.apiProperty("api.contact.name", "Jean Nyilimbibi")
+				.apiProperty("api.license.name", "MIT License")
+				.apiProperty("api.license.url", "https://opensource.org/licenses/MIT")
+                .apiProperty("cors", "true")
 		;
 		
 		/**
 		 * REST endpoint for the Service OpenAPI document 
-		 */
+		 
 		rest().id("openapi-document-restapi")
-			.consumes(MediaType.TEXT_XML)
 			.produces(MediaType.APPLICATION_JSON)
 			
 			// Gets the OpenAPI document for this service
 			.get("/validateMembershipXML/openapi.json")
 				.id("get-openapi-doc-route")
 				.description("Gets the OpenAPI document for this service")
+				.responseMessage()
+					.code(HttpStatus.OK.value())
+					.message(HttpStatus.OK.getReasonPhrase())
+					.responseModel(com.github.jeannyil.fuse.common.models.ValidationResult.class)
+				.endResponseMessage()
 				.route()
 					.log(LoggingLevel.INFO, logName, ">>> ${routeId} - IN: headers:[${headers}] - body:[${body}]").id("log-openapi-doc-request")
 					.setHeader(Exchange.CONTENT_TYPE, constant("application/vnd.oai.openapi+json")).id("set-content-type")
@@ -67,12 +87,14 @@ public class SampleXmlValidationApiRoute extends RouteBuilder {
 					.log(LoggingLevel.INFO, logName, ">>> ${routeId} - OUT: headers:[${headers}] - body:[${body}]").id("log-openapi-doc-response")
 				.end()
 			.endRest()
-		;
+		; */
 		
 		/**
 		 * REST endpoint for the Sample XML Validation RESTful API 
 		 */
 		rest().id("sample-json-validation-restapi")
+			.consumes(MediaType.TEXT_XML)
+			.produces(MediaType.APPLICATION_JSON)
 				
 			// Validates a `Membership` XML instance
 			.post("/validateMembershipXML")
@@ -84,21 +106,29 @@ public class SampleXmlValidationApiRoute extends RouteBuilder {
 					.description("A `Membership` XML instance to be validated.")
 					.dataType("string")
 					.required(true)
+					.example(MediaType.TEXT_XML,
+							 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<p:membership xmlns:p=\"http://www.github.com/jeanNyil/schemas/membership/v1.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <p:requestType>API</p:requestType>\n  <p:requestID>5948</p:requestID>\n  <p:memberID>85623617</p:memberID>\n  <p:status>A</p:status>\n  <p:enrolmentDate>2019-06-29</p:enrolmentDate>\n  <p:changedBy>jeanNyil</p:changedBy>\n  <p:forcedLevelCode>69</p:forcedLevelCode>\n  <p:vipOnInvitation>Y</p:vipOnInvitation>\n  <p:startDate>2019-06-29</p:startDate>\n  <p:endDate>2100-06-29</p:endDate>\n</p:membership>")
 				.endParam()
 				.responseMessage()
 					.code(HttpStatus.OK.value())
 					.message(HttpStatus.OK.getReasonPhrase())
 					.responseModel(com.github.jeannyil.fuse.common.models.ValidationResult.class)
+					.example(MediaType.APPLICATION_JSON, 
+							 "{\n    \"validationResult\": {\n        \"status\": \"OK\"\n    }\n}")
 				.endResponseMessage()
 				.responseMessage()
 					.code(HttpStatus.BAD_REQUEST.value())
 					.message(HttpStatus.BAD_REQUEST.getReasonPhrase())
 					.responseModel(com.github.jeannyil.fuse.common.models.ValidationResult.class)
+					.example(MediaType.APPLICATION_JSON, 
+							 "{\n\t\"validationResult\": {\n\t\t\"status\": \"KO\",\n\t\t\"errorMessage\": \"Validation failed for: com.sun.org.apache.xerces.internal.jaxp.validation.SimpleXMLSchema@5f86796e\\nerrors: [\\norg.xml.sax.SAXParseException: cvc-datatype-valid.1.2.1: '20-06-29' is not a valid value for 'date'., Line : 7, Column : 46\\norg.xml.sax.SAXParseException: cvc-type.3.1.3: The value '20-06-29' of element 'p:enrolmentDate' is not valid., Line : 7, Column : 46\\n]. Exchange[ID-jeansmacbookair-home-1561803539861-1-1]\"\n\t}\n}")
 				.endResponseMessage()
 				.responseMessage()
 					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
 					.message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
 					.responseModel(ErrorResponse.class)
+					.example(MediaType.APPLICATION_JSON, 
+							 "{\n\t\"error\": {\n\t\t\"id\": \"500\",\n\t\t\"description\": \"Internal Server Error\",\n\t\t\"messages\": [\n\t\t\t\"java.lang.Exception: Mocked error message\"\n\t\t]\n\t}\n}")
 				.endResponseMessage()
 				// call the ValidateMembershipXMLRoute
 				.to("direct:validateMembershipXML")
