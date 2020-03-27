6dcbf253d978f9facf45e883380e4c533034c531
jeanNyil <jean.nyilimbibi@gmail.com>
2020-03-28 00:01:49 +0100

    Upgraded to Fuse 7.6 SB-2

--- fuse-common-resources/src/test/java/com/github/jeannyil/fuse/common/routes/test/TestApplication.java
+++ fuse-common-resources/src/test/java/com/github/jeannyil/fuse/common/routes/test/TestApplication.java
@@ -2,12 +2,11 @@ package com.github.jeannyil.fuse.common.routes.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.github.jeannyil.fuse")
-public class TestApplication extends SpringBootServletInitializer {
+public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
--- sample-json-validation-api/src/main/java/com/github/jeannyil/fuse/jsonvalidation/Application.java
+++ sample-json-validation-api/src/main/java/com/github/jeannyil/fuse/jsonvalidation/Application.java
@@ -2,12 +2,11 @@ package com.github.jeannyil.fuse.jsonvalidation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.github.jeannyil.fuse")
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
